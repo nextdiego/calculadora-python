445df15de85f8e02df96451b99c73b3ef76c609c
José Albérico de Oliveira <josealberico@hotmail.com>
2025-08-02 14:55:29 -0300

    divisao de numeros inteiros

--- calculadora.py
+++ calculadora.py
@@ -1,22 +1,19 @@
-def soma(a, b):
+def divisao(a, b):
     """
-    Realiza a soma de dois números.
+    Realiza a divisao de dois números exatos.
     
     Parâmetros:
-    a (int/float): Primeiro número
-    b (int/float): Segundo número
+    a (int/int): Primeiro número
+    b (int/int): Segundo número
     
     Retorna:
-    int/float: Resultado da soma
+    int/int: Resultado da 
     """
-    return a + b
+    return a // b
 
 if __name__ == "__main__":
-    print("Teste da função soma:")
-    print(f"2 + 3 = {soma(2, 3)}") 
-    print(f"5.5 + 4.5 = {soma(5.5, 4.5)}") 
-    print(f'2 + 2 = {soma(2, 2)}')
-    print('que galerinha massa, façam essa conta:')
-    print(f'100 + 100 = {soma(100, 100)}')
-    print(f' 3 + 10 = {soma(3,10)}') 
-    print("vamo genteee (menos ana e seu tema branco)")
+    print("Teste da divisao:")
+    print(f"10 // 5 = {divisao(10, 5)}") 
+    print(f"45 // 9 = {divisao(45, 9)}") 
+    print(f'2 // 2 = {divisao(2, 2)}')
+    print('Espero que seja aprovado!')
