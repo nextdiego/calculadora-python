b57764f506dbdc95ce822e8c0305b0b93db73e38
LuizProCurso <luiz90fernando@gmail.com>
2025-08-02 15:28:06 -0300

    Mult_2

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
-def multiplicação(a, b):
+def soma(a, b):
     """
-    Realiza a multiplicação de dois números.
+    Realiza a soma de dois números.
     
     Parâmetros:
     a (int/float): Primeiro número
@@ -9,12 +9,24 @@ def multiplicação(a, b):
     Retorna:
     int/float: Resultado da soma
     """
-    return a * b
+    return a + b
 
 if __name__ == "__main__":
-    print("Teste da função multiplicação:")
-    print(f"10 * 3 = {multiplicação(10, 3)}") 
-    print(f"8.5 * 4.5 = {multiplicação(8.5, 4.5)}") 
-    print(f'2 * 2 = {multiplicação(2, 2)}')
+    print("Teste da função soma:")
+    print(f"2 + 3 = {soma(2, 3)}") 
+    print(f"5.5 + 4.5 = {soma(5.5, 4.5)}") 
+    print(f'2 + 2 = {soma(2, 2)}')
+    print('que galerinha massa, façam essa conta:')
+    print(f'100 + 100 = {soma(100, 100)}')
+    print(f' 3 + 10 = {soma(3,10)}') 
+    print("vamo genteee (menos ana e seu tema branco)")
+    
+    def multiplicacao (a, b):
+        return a * b
+    print('================Multiplicacao=====================')
+    print("Teste da função multiplicacao:")
+    print(f"10 * 3 = {multiplicacao(10, 3)}") 
+    print(f"8.5 * 4.5 = {multiplicacao(8.5, 4.5)}") 
+    print(f'2 * 2 = {multiplicacao(2, 2)}')
     print("Vs Code modo DARK é melhor!")
-    print('================Multiplicação=====================')
+    
