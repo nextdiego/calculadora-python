d74874a06960e524cafb2f06f8acde2d39659b94
André Filipe Costa <andreppc21@gmail.com>
2025-08-02 11:28:53 -0300

    corrigindo o codigo

--- calculadora.py
+++ calculadora.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
     print("Teste da função soma:")
     print(f"2 + 3 = {soma(2, 3)}") 
     print(f"5.5 + 4.5 = {soma(5.5, 4.5)}") 
-    print(f'2 + 2 = '{soma 2, 2})
+    print(f'2 + 2 = {soma(2, 2)}')
